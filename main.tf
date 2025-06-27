e299fc0729660af9ede75acaaf8369c03654bfb0
kgmdevops <kgmimsdevops@kgisl.microcollege.in>
2025-06-27 14:42:49 +0530

    add another rule

--- main.tf
+++ main.tf
@@ -61,6 +61,12 @@ resource "aws_security_group" "tf_sg" {
     protocol        = "tcp"
     cidr_blocks     = ["0.0.0.0/0"]
   }
+    ingress {
+    from_port       = 2222
+    to_port         = 2222
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
   egress {
     from_port        = 0
     to_port          = 0
